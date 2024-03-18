7987e9dd3ff9de35dd9f0cf77701cd1a39c892c9
Jason <102817168+SloKart@users.noreply.github.com>
2024-03-18 14:54:03 -0400

    plugin added

---
+++ src/config/plugins/eleventy-plugin-pwa-v2.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginPWA, {
+    cacheId: "KainoGen", // change this to your application id
+    globIgnores: [
+      // any files you don't want service worker to cache go here
+      "share-*.jpg",
+    ],
+    runtimeCaching: [
+      {
+        // we always want fresh copy of the index page
+        urlPattern: /\/$/,
+        handler: "NetworkFirst",
+      },
+      {
+        // we also want fresh copies of any HTML page
+        urlPattern: /\.html$/,
+        handler: "NetworkFirst",
+      },
+      {
+        // we serve stale copies of static assets while they're refreshed
+        urlPattern:
+          /^.*\.(jpg|png|mp4|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/,
+        handler: "StaleWhileRevalidate",
+      },
+    ],
+  });
+};
