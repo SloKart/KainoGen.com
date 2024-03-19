25711b30b6a2e2c03319026286d2f596e1586a22
Jason <102817168+SloKart@users.noreply.github.com>
2024-03-19 16:00:33 -0400

    taking the brakes off of eleventy javascript bundle

--- src/config/eleventy/javascript.js
+++ src/config/eleventy/javascript.js
@@ -9,18 +9,18 @@ module.exports = {
     // By default, all JS files in assets will be compiled and put in /public.
     // If you only want bundled files to be compiled/served, uncomment the below two lines, and list the files you wish to be bundled in the array
 
-    const filesToBundle = [
-      "./src/assets/js/app.js",
-      "./src/assets/js/prices.js",
-      "./src/assets/js/shopify.js",
-
-    ];
-    if (!filesToBundle.includes(inputPath)) return;
+    // const filesToBundle = [
+    //   "./src/assets/js/app.js",
+    //   "./src/assets/js/prices.js",
+    //   "./src/assets/js/shopify.js",
+    //   "*/service-worker.js",
+    // ];
+    // if (!filesToBundle.includes(inputPath)) return;
 
     // If the file isn't from the assets directory, or is an 11tydata file, ignore it. It's probably a config file.
     if (
       !inputPath.includes("./src/assets/") ||
-      inputPath.includes("11tydata.js")
+      inputPath.includes("11tydata.js") 
     ) {
       return;
     }
